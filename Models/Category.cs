d94c66e4f4ef4cb9de41268dcc1551efc96ff9d4
ibo215 <mashal200315@gmail.com>
2025-05-20 11:51:45 +0300

    Add project files.

---
+++ Models/Category.cs
@@ -0,0 +1,12 @@
+﻿namespace Zulfikar.Solar.API.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        // Navigation
+        public List<Product> Products { get; set; }
+    }
+
+}
---
+++ Models/Product.cs
@@ -0,0 +1,16 @@
+﻿namespace Zulfikar.Solar.API.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+        public decimal Price { get; set; }
+        public int CategoryId { get; set; }
+
+        // Navigation
+        public Category Category { get; set; }
+    }
+
+}
